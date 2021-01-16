3c7b839181ad92f412917e81ec62b4751c373e5a
bhavini <bhavini266@gmail.com>
2021-01-15 18:57:05 -0600

    added insert and other required queries

--- sql_queries.py
+++ sql_queries.py
@@ -58,3 +58,36 @@ month int,
 year int,
 weekday varchar)
 """)
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+INSERT INTO songplay (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s,%s,%s,%s,%s, %s,%s,%s)
+""")
+
+user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s,%s,%s,%s,%s)
+ON CONFLICT (user_id) DO UPDATE set level = EXCLUDED.level
+""")
+
+song_table_insert = (""" INSERT INTO song (song_id, title, artist_id, year, duration) VALUES (%s,%s,%s,%s,%s)
+ON CONFLICT (song_id) DO NOTHING
+""")
+
+artist_table_insert = (""" INSERT INTO artist (artist_id, artist_name, artist_location,  latitude, longitude)
+VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING
+""")
+
+time_table_insert = (""" INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING
+""")
+
+# FIND SONGS
+
+song_select = (""" SELECT s.song_id, a.artist_id FROM song s JOIN artist a ON s.artist_id = a.artist_id
+WHERE s.title = %s and a.artist_name = %s and s.duration = %s """)
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
